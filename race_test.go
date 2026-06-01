5433f993d07953a7c04d2f70fded00f2c4c7e46b
Moksh Chadha <mokshchadha@Mokshs-MacBook-Pro.local>
2026-06-01 19:34:02 +0530

    completed dreaft one of concurrency

--- race_test.go
+++ race_test.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"sync/atomic"
 	"testing"
 )
 
-func TestDataRaceConditions(t *testing.T) {
+//	func TestDataRaceConditions(t *testing.T) {
+//		// race condition - when 2 threads try to write to or read same variable
+//		//
+//		var state int32
+//		var mu sync.RWMutex
+//		// go x
+//		// go y
+//		fmt.Println(state)
+//		for i := 0; i < 10; i++ {
+//			go func(i int) {
+//				mu.Lock()
+//				state += int32(i)
+//				mu.Unlock()
+//			}(i)
+//		}
+//	}
+func TestDataRaceConditionsAtomicValue(t *testing.T) {
 	// race condition - when 2 threads try to write to or read same variable
 	//
 	var state int32
-	var mu sync.RWMutex
+
 	// go x
 	// go y
 	fmt.Println(state)
 	for i := 0; i < 10; i++ {
 		go func(i int) {
-			mu.Lock()
-			state += int32(i)
-			mu.Unlock()
+			atomic.AddInt32(&state, int32(i))
 		}(i)
 	}
 }
